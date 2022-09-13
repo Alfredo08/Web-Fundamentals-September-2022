d8e1a243b40498854f3a19799bf6457d2b63d65c
Alfredo08 <alfredosv08@gmail.com>
2022-09-13 15:49:44 -0500

    Adding CSS

---
+++ Week 1/Day 3/Algos.js
@@ -0,0 +1,30 @@
+/**
+ * Finds the longest word in the given array of words.
+ * @param {Array<string>} words An array of strings.
+ * @returns {string} The longest word. If there are multiple words with the same
+ *    length, this should be the last word in the array with that length.
+ */
+ function findLongestWord( words ) {
+
+ }
+
+/**
+ * Calculates the sum of the given range, inclusive. I.e., the sum of the first
+ * number through the last number, inclusive.
+ * @param {number} lowestNum The lowest number in the range.
+ * @param {number} highestNum The highest number in the range.
+ * @returns {number} The sum of the given range, inclusive.
+ */
+ function inclusiveRangeSum( lowestNum, highestNum ) {
+
+ }
+
+/**
+ * Finds the index from the given array where the given item is found.
+ * @param {Array<any>} items An array of any kind of items.
+ * @param {any} searchItem The item to find.
+ * @returns {number} The index of found item, or -1 if not found.
+ */
+ function indexOf(items, searchItem) {
+    
+ }
