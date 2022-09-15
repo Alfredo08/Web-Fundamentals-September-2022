3b1d45e456a3845e33462e0fe33a33412ae485c8
Alfredo08 <alfredosv08@gmail.com>
2022-09-15 11:44:36 -0500

    Positioning

---
+++ Week 1/Day 4/Algos.js
@@ -0,0 +1,22 @@
+/**
+ * Retrieves the nth to last indexed item from the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<any>} items An array of any kind of items.
+ * @param {number} nthToLast
+ * @returns {any} The item at the nthToLast index or null.
+ */
+ function nthLast(items, nthToLast) {
+    // code here
+ }
+
+/**
+ * Counts how many numbers are both even and negative.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} nums
+ * @returns {number} The count.
+ */
+function countEvenNegatives(nums) {
+    // code here
+}
